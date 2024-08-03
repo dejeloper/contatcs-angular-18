b25cfeb934739dea9007c44f1a22d569a64d0a2a
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2024-08-03 16:11:43 -0500

    Add ToolbarComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ToolbarComponent } from '@components/toolbar/toolbar.component';
+import { MatCardModule } from '@angular/material/card';
+
+const MATERIAL_MODULES = [MatCardModule];
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ToolbarComponent, MATERIAL_MODULES],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Demo Angular';
+
+  onClickNewContact(): void {
+    console.log('Modal');
+  }
 }
