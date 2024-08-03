7a2e554b9e5c8a61bc3203d55ec1dc566873d244
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2024-08-03 13:56:24 -0500

    Add firebase

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,1 +1,11 @@
-export const environment = {};
+export const environment = {
+  firebase: {
+    projectId: 'fir-angular-d68c2',
+    appId: '1:258932267382:web:be924106572ddad2ae4257',
+    storageBucket: 'fir-angular-d68c2.appspot.com',
+    apiKey: 'AIzaSyB2xyonZi4OWEyURPky2E6Up3-TmBz65dA',
+    authDomain: 'fir-angular-d68c2.firebaseapp.com',
+    messagingSenderId: '258932267382',
+    measurementId: 'G-FEYP5RGQ5C',
+  },
+};
